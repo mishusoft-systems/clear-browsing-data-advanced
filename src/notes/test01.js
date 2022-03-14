e22e8a420788690436d95c34574fe263e7259a2e
Al-Amin Ahamed <alamin@mishusoft.com>
2022-03-14 13:55:37 +0600

    Update test01.js

--- src/notes/test01.js
+++ src/notes/test01.js
@@ -1,14 +1,32 @@
 $(document).ready(function () {
     // viewer
     let viewerTag = document.createElement('ol');
-    $("#table_content_title").append(viewerTag);
+    viewerTag.setAttribute('style', 'list-style:none')
 
-    if ($(".et_pb_post_content").childNodes.length !== 0) {
-        $(".et_pb_post_content").childNodes.forEach((element, index) => {
+    // append to main container
+    document.querySelector('#table_content_title').appendChild(viewerTag);
+
+    // datta container
+    const bodyContent = document.querySelector('.et_pb_post_content');
+
+    if (bodyContent.childElementCount !== 0) {
+        bodyContent.childNodes.forEach((element, index) => {
             // h2 tag = element
             if (element.nodeName.toString().indexOf('H') !== -1) {
-                element.setAttribute('id', 'id' + index);
-                viewerTag.append('<li><a  href="#id"'+index + '> ' + element.textContent + '</a></li>');
+                const navigation = 'id-' + index;
+                element.setAttribute('id', navigation);
+
+                // new anchor child
+                let anchorElement = document.createElement('a');
+                anchorElement.setAttribute('href', '#'+ navigation);
+                anchorElement.textContent = element.textContent
+
+                // new list item child
+                let listItemElement  = document.createElement('li');
+                listItemElement.appendChild(anchorElement);
+
+                //append child to viewer
+                viewerTag.appendChild(listItemElement);
             }
         })
     }
