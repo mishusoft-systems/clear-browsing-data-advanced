be6b2903fe58a50d759e72c197c00759b59b9039
Al-Amin Ahamed <alamin@mishusoft.com>
2022-03-14 19:38:19 +0600

    Updates

--- src/notes/test01.js
+++ src/notes/test01.js
@@ -30,4 +30,19 @@ $(document).ready(function () {
             }
         })
     }
+
+
+    // make ready list viewer
+    document.querySelector('#table_content_title').append('<ol id="outline-list" style="list-style: none"> </ol>');
+
+    // datta container
+    document.querySelector('.et_pb_post_content').children.each(function (index, element) {
+        // h2 tag = element
+        if (element.nodeName.indexOf('H') !== -1) {
+            element.setAttribute('id', 'id-' + index);
+
+            //append child to viewer
+            document.querySelector('#outline-list').append('<li><a href="#id-' + index + '">' + element.textContent + '</a></li>');
+        }
+    });
 });
